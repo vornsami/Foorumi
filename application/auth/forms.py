96917adfa3d590c39882d30458f4d6232ce70661
vornsami <sami.vornanen@helsinki.fi>
2019-05-04 19:09:20 +0300

    Lisätty ominaisuuksia

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -7,3 +7,15 @@ class LoginForm(FlaskForm):
   
     class Meta:
         csrf = False
+
+class NameForm(FlaskForm):
+    username = StringField("Username", [validators.Length(min=3)])
+  
+    class Meta:
+        csrf = False
+
+class PasswordForm(FlaskForm):
+    password = PasswordField("Password", [validators.Length(min=5)])
+  
+    class Meta:
+        csrf = False
--- application/auth/views.py
+++ application/auth/views.py
@@ -3,7 +3,7 @@ from flask_login import login_user, logout_user, login_required, current_user
 
 from application import app, db
 from application.auth.models import User
-from application.auth.forms import LoginForm
+from application.auth.forms import LoginForm, NameForm, PasswordForm
 from application.tasks.models import Thread
 
 @app.route("/kirjaudu", methods = ["GET", "POST"])
@@ -71,10 +71,13 @@ def userpage():
 @login_required
 def newname():
     if request.method == "GET":
-        return render_template("auth/newname.html", form = LoginForm())
+        return render_template("auth/newname.html", form = NameForm())
 		
-    form = LoginForm(request.form)
+    form = NameForm(request.form)
     
+    if not form.validate():
+        return render_template("auth/newname.html", form = form,
+                               error = "Invalid name")
 	
     user = User.query.filter_by(username=form.username.data).first()
     if user and not form.username.data == current_user.username:
@@ -90,3 +93,33 @@ def newname():
     db.session().commit()
     
     return redirect(url_for("userpage"))
+	
+@app.route("/newpassword", methods = ["GET", "POST"])
+@login_required
+def newpassword():
+    if request.method == "GET":
+        return render_template("auth/newpassword.html", form = PasswordForm())
+		
+    form = PasswordForm(request.form)
+	
+    if not form.validate():
+        return render_template("auth/newpassword.html", form = form,
+                               error = "Invalid password")
+							   
+    current_user.password = form.password.data;
+    db.session().commit()
+    
+    return redirect(url_for("userpage"))
+	
+@app.route("/deleteUser", methods = ["POST"])
+@login_required
+def deleteUser():
+    
+    db.session().delete(current_user)
+    db.session().commit()
+	
+    return redirect(url_for("aloitus"))
+    
+
+    
+
